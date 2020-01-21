7bb43968c76dc55b0c3fee307e70d44bec38d576
Ning-S <ningxianwen.n@gmail.com>
2020-01-21 13:44:31 +0800

    tushare get started,first program

--- my_test/tushare__start.py
+++ my_test/tushare__start.py
@@ -5,8 +5,9 @@ import tushare as ts
 pingan_code='601318'
 shangqi_code='600104'
 icbc_code='601398'
-real_time=ts.get_realtime_quotes([pingan_code,shangqi_code,icbc_code])
-
-print(real_time)
+huada_genic='300676'
+real_time=ts.get_realtime_quotes([pingan_code,shangqi_code,icbc_code,huada_genic])
+need_data=real_time[['name','open','price','high','low','bid','volume','date','time']]
+print(need_data)
 
 
